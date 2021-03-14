8f9b3d793bd55926c8f98fc2728fe5ad70bb375c
zgaaaa <1924297895@qq.com>
2021-03-14 13:56:38 +0800

    master

---
+++ link/025.go
@@ -0,0 +1,6 @@
+package link
+
+// type ListNode struct {
+//    Val int
+//    Next *ListNode
+// }
---
+++ link/206.go
@@ -0,0 +1,20 @@
+package link
+
+/*
+反转一个单链表。
+
+示例:
+输入: 1->2->3->4->5->NULL
+输出: 5->4->3->2->1->NULL
+*/
+
+// type ListNode struct {
+//    Val int
+//    Next *ListNode
+// }
+
+func ReverseList(head *ListNode) *ListNode {
+	 := new(ListNode)
+	
+
+}
