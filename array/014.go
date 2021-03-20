6fe149f594ffb044258f5c0f4a9c84611d2ae0d7
zgaaaa <1924297895@qq.com>
2021-03-20 23:07:00 +0800

    master

--- array/014.go
+++ array/014.go
@@ -1,13 +1,18 @@
-package main
+package array
 
-import (
-	"fmt"
-)
+/*
+编写一个函数来查找字符串数组中的最长公共前缀。
+如果不存在公共前缀，返回空字符串 ""。
 
-func main() {
-	str := []string{"ab", "a"}
-	fmt.Println(LongestCommonPrefix(str))
-}
+示例 1：
+输入：strs = ["flower","flow","flight"]
+输出："fl"
+
+示例 2：
+输入：strs = ["dog","racecar","car"]
+输出：""
+解释：输入不存在公共前缀。
+*/
 
 func LongestCommonPrefix(strs []string) string {
 	if len(strs) == 0 {
@@ -21,7 +26,7 @@ func LongestCommonPrefix(strs []string) string {
 			// 边界条件,
 			// 1.当第i个字符串的第j个字符与第0个字符串的第j个字符不相等的时候
 			// 2.由于外层循环是按照第0个字符串的长度遍历的,有可能后面的字符串没有第0个字符串长
-			if j == len(strs[i]) || strs[i][j] != strs[0][j] {
+			if strs[i][j] != strs[0][j] || j > len(strs[i]) {
 				return strs[0][:j]
 			}
 		}
