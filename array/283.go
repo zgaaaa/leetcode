b5d2ebed36c0179476abb69a2d99d08c12e85121
zgaaaa <1924297895@qq.com>
2021-03-21 16:46:22 +0800

    master

---
+++ array/283.go
@@ -0,0 +1,30 @@
+package array
+
+/*
+给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+
+示例:
+
+输入: [0,1,0,3,12]
+输出: [1,3,12,0,0]
+说明:
+
+必须在原数组上操作，不能拷贝额外的数组。
+尽量减少操作次数。
+*/
+
+func MoveZeroes(nums []int)  {
+	res := 0
+	length := len(nums)
+	// 将不为零的值都移到前面
+	for i := 0; i < length; i++ {
+		if nums[i] != 0 {
+			nums[res] = nums[i]
+			res++
+		}
+	}
+	// 将后面的数冲零
+	for i := res; i < length; i++ {
+		nums[i] = 0
+	}
+}
