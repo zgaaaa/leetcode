bd84d1dd60777943a6e0300d34e03afcbc419f5b
zgaaa <1924297895@qq.com>
2021-03-22 23:27:16 +0800

    master

--- main.go
+++ main.go
@@ -1,23 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	slice := []int{2, 2, 3}
-	fmt.Println(removeDuplicates(slice))
 
-}
-
-func removeDuplicates(nums []int) int {
-	i := 0
-	j := 0
-	for i < len(nums)-1 {
-		for nums[i] == nums[i + 1] {
-			nums = append(nums[:i + 1], nums[i + 2:]...)
-			j++
-		}
-	}
-	return i + 1
 }
