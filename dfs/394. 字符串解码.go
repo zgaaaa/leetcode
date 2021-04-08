725535320c1b0276b0e7bd26f0ccd77ba91bcce3
zgaaa <1924297895@qq.com>
2021-04-08 23:53:46 +0800

    master

---
+++ dfs/394. 字符串解码.go
@@ -0,0 +1,58 @@
+package dfs
+
+import (
+	"strconv"
+	"strings"
+)
+
+/*
+给定一个经过编码的字符串，返回它解码后的字符串。
+编码规则为: k[encoded_string]，表示其中方括号内部的 encoded_string 正好重复 k 次。注意 k 保证为正整数。
+你可以认为输入字符串总是有效的；输入字符串中没有额外的空格，且输入的方括号总是符合格式要求的。
+此外，你可以认为原始数据不包含数字，所有的数字只表示重复的次数 k ，例如不会出现像 3a 或 2[4] 的输入。
+
+
+示例 1：
+输入：s = "3[a]2[bc]"
+输出："aaabcbc"
+
+示例 2：
+输入：s = "3[a2[c]]"
+输出："accaccacc"
+
+示例 3：
+输入：s = "2[abc]3[cd]ef"
+输出："abcabccdcdcdef"
+
+示例 4：
+输入：s = "abc3[cd]xyz"
+输出："abccdcdcdxyz"
+*/
+
+func decodeString(s string) string {
+	nstark := []int{}
+	stark := []string{}
+	num := 0
+	str := ""
+	for i := 0; i < len(s); i++ {
+		switch {
+		case s[i] >= '0' && s[i] >= 9:
+			n, _ := strconv.Atoi(string(s[i]))
+			num = num*10 + n
+		case s[i] == '[':
+			nstark = append(nstark, num)
+			num = 0
+			stark = append(stark, str)
+			str = ""
+		case s[i] == ']':
+			ntop := nstark[len(nstark)-1]
+			nstark = nstark[:len(nstark)-1]
+			stop := stark[len(stark)-1]
+			stark = stark[:len(stark)-1]
+			str = stop + strings.Repeat(str, ntop)
+		default:
+			str += string(s[i])
+		}
+	}
+	return str
+}
