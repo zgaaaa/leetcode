512cb6665082e0e06448e3742d6e2d7e8552f781
zgaaaa <1924297895@qq.com>
2021-04-13 18:47:48 +0800

    master

--- twopointers/142.环形链表Ⅱ.go
+++ twopointers/142.环形链表Ⅱ.go
@@ -2,13 +2,13 @@ package twopointers
 
 /*
 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
-为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 
+为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。
 如果 pos 是 -1，则在该链表中没有环。注意，pos 仅仅是用于标识环的情况，并不会作为参数传递到函数中。
 说明：不允许修改给定的链表。
 
 进阶：
 你是否可以使用 O(1) 空间解决此题？
- 
+
 
 示例 1：
 输入：head = [3,2,0,-4], pos = 1
@@ -24,8 +24,25 @@ package twopointers
 输入：head = [1], pos = -1
 输出：返回 null
 解释：链表中没有环。
-*/
 
-func detectCycle(head *ListNode) *ListNode {
-    
+题解:https://leetcode-cn.com/problems/linked-list-cycle-ii/solution/linked-list-cycle-ii-kuai-man-zhi-zhen-shuang-zhi-/
+*/
+// 快慢指针
+func DetectCycle(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next      // 慢指针
+		fast = fast.Next.Next // 快指针
+		if slow == fast {     // 当快指针和慢指针相遇时,表名有环
+			// 快指针从头开始,移动步长为1
+			// 当快慢指针再次相遇时,所指的位置就是环的入口
+			fast = head
+			for fast != slow {
+				fast = fast.Next
+				slow = slow.Next
+			}
+			return fast
+		}
+	}
+	return nil
 }
