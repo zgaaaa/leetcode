93c1f9987fe4427b8dd1fadda4d465cd991ea326
zgaaaa <1924297895@qq.com>
2021-04-20 14:54:11 +0800

    master

---
+++ link/160.相交链表.go
@@ -0,0 +1,20 @@
+package link
+
+// 编写一个程序，找到两个单链表相交的起始节点。
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	lista, listb := headA, headB
+	for lista != listb {
+		if lista != nil {
+			lista = lista.Next
+		}else {
+			lista = headB
+		}
+		if listb != nil {
+			listb = listb.Next
+		}else {
+			listb = headA
+		}
+	}
+	return lista
+}
