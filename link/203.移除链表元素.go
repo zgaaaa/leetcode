f800267ea9b7866741fc038af4b4555c124d0ef5
zgaaaa <1924297895@qq.com>
2021-04-20 17:38:01 +0800

    master

---
+++ link/203.移除链表元素.go
@@ -0,0 +1,19 @@
+package link
+
+/*
+给你一个链表的头节点 head 和一个整数 val ，请你删除链表中所有满足 Node.val == val 的节点，并返回 新的头节点 。
+*/
+
+func removeElements(head *ListNode, val int) *ListNode {
+	res := &ListNode{Next: head}
+	pre := res
+	for head != nil {
+		if head.Val == val {
+			pre.Next = head.Next
+		} else {
+			pre = pre.Next
+		}
+		head = head.Next
+	}
+	return res.Next
+}
