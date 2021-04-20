ae343e633f5d917cdad3c61d89a7be0a7509df85
zgaaaa <1924297895@qq.com>
2021-04-20 13:22:23 +0800

    master

---
+++ link/83. 删除排序链表中的重复元素.go
@@ -0,0 +1,29 @@
+package link
+
+/*
+存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次 。
+返回同样按升序排列的结果链表。
+
+示例 1：
+输入：head = [1,1,2]
+输出：[1,2]
+
+示例 2：
+输入：head = [1,1,2,3,3]
+输出：[1,2,3]
+*/
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	res := &ListNode{Next: head}
+	for head.Next != nil {
+		if head.Val == head.Next.Val {
+			head.Next = head.Next.Next
+		} else {
+			head = head.Next
+		}
+	}
+	return res.Next
+}
