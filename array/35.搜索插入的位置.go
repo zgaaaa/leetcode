d6c3f23eae4a90b493eea3ceb412cbb7d9126850
zgaaa <1924297895@qq.com>
2021-04-25 16:09:46 +0800

    master

--- array/35.搜索插入的位置.go
+++ array/35.搜索插入的位置.go
@@ -36,3 +36,18 @@ func SearchInsert(nums []int, target int) int {
 	}
 	return l
 }
+// 方法二
+func SearchInsert2(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := (l + r) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
+	return l
+}
