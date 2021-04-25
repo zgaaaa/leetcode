f335cda12581d7ff4d1268d248e02767c8c1dcce
zgaaa <1924297895@qq.com>
2021-04-25 16:09:51 +0800

    master

--- array2/035.go
+++ array2/035.go
@@ -1,5 +1,20 @@
 package array2
 
+// 示例 1:
+// 输入: [1,3,5,6], 5
+// 输出: 2
+
 func SearchInsert(nums []int, target int) int {
-	
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := (l + r) / 2
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
+	return l
 }
