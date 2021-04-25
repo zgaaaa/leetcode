cd3d532203f9d59861146a882f0a7e8a441b0db3
zgaaa <1924297895@qq.com>
2021-04-25 12:26:17 +0800

    master

---
+++ array2/015.go
@@ -0,0 +1,26 @@
+package array2
+
+import "sort"
+
+// 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？
+// 请你找出所有和为 0 且不重复的三元组。
+// 注意：答案中不可以包含重复的三元组。
+// 示例 1：
+// 输入：nums = [-1,0,1,2,-1,-4]
+// 输出：[[-1,-1,2],[-1,0,1]]
+
+
+func ThreeSum(nums []int) [][]int {
+	sort.Ints(nums)
+	res := [][]int{}
+	length := len(nums)
+	if length < 3 {
+		 return res
+	}
+	for i, v := range nums {
+		if v > 0 {
+			return res
+		}
+		
+	}
+}
